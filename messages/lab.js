b564a15371b33999b941c80396af2b3b54e9f0d3
Nicholas Yan <nicholasyan@users.noreply.github.com>
2015-03-04 23:06:54 -0500

    first draft - all features (theoretically) added

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+function parse() {
+    
+    // step 1: create an instance of the object
+    var request = new XMLHttpRequest();
+
+    // step 2: create or "open" HTTP request
+    request.open("GET", "data.json", true);
+
+    // step 3: set up way to manage response - to a function
+    request.onreadystatechange = parseData;
+
+    // step 4: execute the request
+    request.send();
+
+}
+
+function parseData() {
+
+	message_to_add = JSON.parse();
+
+	message_list = document.getElementById("messages").value;
+	
+	message_list.innerHTML += '<p class="message">' + message_to_add['content'] + " " + message_to_add['username'] "</p>";
+}
