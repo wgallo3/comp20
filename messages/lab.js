8e9daa60b60814d222a59b1a6c8290e2a94dd6b7
Nicholas Yan <nicholasyan@users.noreply.github.com>
2015-03-05 00:25:18 -0500

    passing paramters

--- messages/lab.js
+++ messages/lab.js
@@ -1,33 +1,34 @@
 function parse() {
     
     // step 1: create an instance of the object
-    var request = new XMLHttpRequest();
+    request = new XMLHttpRequest();
 
     // step 2: create or "open" HTTP request
     request.open("GET", "http://messagehub.herokuapp.com/messages.json", true);
 
     // step 3: set up way to manage response - to a function
-    request.onreadystatechange = parseData;
+    request.onreadystatechange = parseData(request);
 
     // step 4: execute the request
     request.send();
 
-	function parseData() {
+}
 
-		if (request.readyState == 4 && request.status == 200) {
 
-			var message = JSON.parse(request.responseText);
+function parseData(request) {
 
-			message_list = document.getElementById("messages");
+	if (request.readyState == 4 && request.status == 200) {
 
-			for (var i = 0; i < message.length; i++) {
+		var message = JSON.parse(request.responseText);
 
-				message_list.innerHTML += '<p class="message">' + message[i]['content'] + ' ' + message[i]['username'] + '</p>';
+		message_list = document.getElementById("messages");
 
-			}
-			
+		for (var i = 0; i < message.length; i++) {
+
+			message_list.innerHTML += '<p class="message">' + message[i]['content'] + ' ' + message[i]['username'] + '</p>';
+
 		}
-
+		
 	}
 
 }
